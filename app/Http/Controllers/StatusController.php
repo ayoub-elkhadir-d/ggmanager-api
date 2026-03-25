38e47dfb493e8f60f00e81f02073d794af1f527b
yassinmftah <yassinmaftah06@gmail.com>
2026-03-25 09:31:17 +0100

    add status controller

---
+++ app/Http/Controllers/StatusController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Tournament;
+use Illuminate\Http\Request;
+
+class StatusController extends Controller
+{
+    public function close(Request $request, Tournament $tournament)
+    {
+        $user = $request->user();
+
+        if ($user->cannot('update', $tournament)) {
+            return response()->json(['message' => 'You are not allowed to do this.'], 403);
+        }
+
+        if ($tournament->status !== 'open')
+            return response()->json(['message' => 'Only open tournaments can be closed.'], 400);
+
+        $tournament->update(['status' => 'closed']);
+
+        return response()->json(['message' => 'Tournament has been closed successfully.'], 200);
+    }
+}
